a9283d66ca22b9aebff0a5acc97e8e4d98a7a4b5
류호성 <52982845+Hoseong-Ryu@users.noreply.github.com>
2021-08-30 20:28:41 +0900

    Time: 172 ms (83.93%), Space: 36.4 MB (39.29%) - LeetHub

---
+++ number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun numOfStrings(patterns: Array<String>, word: String): Int {
+        var count = 0
+        for (i in patterns){
+            if (word.contains(i)){
+                count++
+            }
+        }
+        return count
+    }
+}
